c95a00357630fa2dcc2de5bf96c9f07909282857
Andre Derme <andredermee@gmail.com>
2025-08-05 15:40:39 -0300

    Create ex 20.py

---
+++ ex 20.py
@@ -0,0 +1,8 @@
+m1 = float (input ('Qual sua media da UN1 ?'))
+m2 = float (input ('Qual sua nota da UN2 ?'))
+
+media = (m1 + m2)/2
+if media >= 6:
+    print (' \033[32maprovado !!!')
+else:
+    print (' \033[31mREPROVADO !!')

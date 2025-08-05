ef6082ed9b95bda3eb64a1a25afaae33ab88fb83
Andre Derme <andredermee@gmail.com>
2025-08-05 15:40:58 -0300

    Create ex 26.py

---
+++ ex 26.py
@@ -0,0 +1,9 @@
+#aula 38
+n1 =  int(input ('digite um numero:'))
+n2 = int (input ('digite o seugundo numero:'))
+if n1 > n2 :
+    print ('O primeiro numero é Maior')
+elif n2 > n1:
+    print ('o segundo valor é Maior')
+elif n1==n2:
+    print ('Os dois são iguais')

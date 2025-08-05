496dcca1a24c15e34ceae1bd8fab39cc9ef06a9e
Andre Derme <andredermee@gmail.com>
2025-08-05 15:40:51 -0300

    Create ex 23.py

---
+++ ex 23.py
@@ -0,0 +1,7 @@
+import random 
+n1 = ('ela te ama ')
+n2 =  ('ela n te ama')
+n3 = ('talvez dê certo ')
+lista = [n1, n2,  n3]
+escolhido = random.choice(lista)
+print (' seu destino foi {}'. format (escolhido))

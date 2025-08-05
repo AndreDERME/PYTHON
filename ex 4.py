e0ab186440ea73e3584e0b2336e64269e72d0dd8
Andre Derme <andredermee@gmail.com>
2025-08-05 15:41:34 -0300

    Create ex 4.py

---
+++ ex 4.py
@@ -0,0 +1,2 @@
+n = int (input('Digite um nmero:'))
+print ('Analisando sua escolha, o dobro é {1}, o triplo é {2} e o numero multiplicado por ele mesmo resulta em {3 2}'.format (n, (n * 2), (n * 3), (n ** 2 )))

667ab8942e7051e7fd4a1d64a39f8052c6fade45
Andre Derme <andredermee@gmail.com>
2025-08-05 15:41:08 -0300

    Create ex 3.py

---
+++ ex 3.py
@@ -0,0 +1,6 @@
+n1 = int (input('escolha um numero:'))
+n2 = int (input ('escolha outro numero:'))
+s = n1 + n2 
+quadrado = s **2
+print ('o resultado entre {0} e {1} vale {2}' . format ( n1, n2, s)) 
+print ('se essa soma for elevada ao quadrado, terá o resultado de', quadrado )

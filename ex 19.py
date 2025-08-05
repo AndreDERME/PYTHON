1d8ee3e9df2396506638129069fcc813881596f2
Andre Derme <andredermee@gmail.com>
2025-08-05 15:40:34 -0300

    Create ex 19.py

---
+++ ex 19.py
@@ -0,0 +1,6 @@
+nome = str (input(' Qual seu nome ?'))
+if nome == 'Gustavo':
+    print ('que nome bonito você tem!!!')
+else:
+    print ('seu nome é tão normal!')
+print (f'Tenha um bom dia {nome}!!!') 
